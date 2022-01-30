fd9d646aa8b047d6b3fbd70d11cca82fb57de581
Alejandro <alex_carrasco17@hotmail.com>
2022-01-30 01:15:48 -0700

    Initial version of my Pokedex

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+
+//Pages
+import Homepage from './pages/Homepage';
+import PokemonPage from './pages/PokemonPage';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <Router>
+      <Header/>
+      <Container>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/pokemon/:id' component={PokemonPage}/>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
